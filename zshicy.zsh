0690b95cf18de315d93cf6893713a2581bde11b0
tlneondo <tlneondo@gmail.com>
2025-09-03 18:02:14 -0400

    clean up zsh config

--- zshicy.zsh
+++ zshicy.zsh
@@ -1,20 +1,24 @@
 ZSH_THEME="juanghurtado"
 
-alias fedupdate-grub="sudo grub2-mkconfig -o /boot/grub2/grub.cfg"
+#run dnf and flatpak update
 alias fedupdate="sudo dnf upgrade && flatpak update"
+
+#boot to windows
 alias wreboot="pkexec ~/Projects/WinWalkAway/winwalkaway.sh"
-#alias archupdate="yay -Syu && flatpak update "
 
-#old script
-#alias syncWindowsGames="/home/icyjiub/Scripts/syncWindowsGames/syncWindowsGames.sh"
 #overlayfs sync script
-alias syncWindowsGames="sudo bash /home/icyjiub/Projects/SteamOverlayFS/SteamOverlayFS/SteamOverlaySync.sh"
+alias syncWindowsGames="sudo bash ~/Projects/SteamOverlayFS/SteamOverlayFS/SteamOverlaySync.sh"
 
-alias SwitchToTV="/home/icyjiub/Scripts/SwitchToTV.sh"
-alias SwitchToDesk="/home/icyjiub/Scripts/SwitchToDesk.sh"
-alias singleScreenVRR="/home/icyjiub/Scripts/singleScreenVRR.sh"
+
+#monitor control
+alias SwitchToTV="~/Scripts/SwitchToTV.sh"
+alias SwitchToDesk="~/Scripts/SwitchToDesk.sh"
+alias singleScreenVRR="~/Scripts/singleScreenVRR.sh"
 alias disableVRR="kscreen-doctor output.DP-1.vrrpolicy.never"
 alias disablesidemonitor="kscreen-doctor output.DP-3.disable"
 alias enableVRR="kscreen-doctor output.DP-1.vrrpolicy.automatic"
 alias enablesidemonitor="kscreen-doctor output.DP-3.enable"
-alias update-grub="sudo grub2-mkconfig -o /boot/grub2/grub.cfg"
+
+#grub update
+alias update-grub="sudo grub-mkconfig -o /boot/grub/grub.cfg"
+alias update-grub2="sudo grub2-mkconfig -o /boot/grub2/grub.cfg"
