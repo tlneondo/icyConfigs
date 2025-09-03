0816f49611132f11cf3c1240c734a56cb3bc5cb5
tlneondo <tlneondo@gmail.com>
2025-09-03 17:56:33 -0400

    initialconfigs

---
+++ installconfigs.sh
@@ -0,0 +1,9 @@
+#/usr/bin/bash
+
+#zshconfig
+ln ./zshicy.zsh ~/.oh-my-zsh/custom/zshicy.zsh
+
+
+#mpv config
+ln ./mpv/mpv.conf ../.config/mpv/mpv.conf 
+ln ./mpv/input.conf ../.config/mpv/input.conf
